939f5e7d567aee235f4122badc57705e4e9ca2d6
bakins <brian.akins@turner.com>
2013-10-21 08:04:19 -0400

    bump iteration

--- config/projects/nginx.rb
+++ config/projects/nginx.rb
@@ -6,7 +6,7 @@ homepage "http://openresty.org"
 replaces        "nginx"
 install_path    "/opt/nginx"
 build_version   '1.4.2.9'
-build_iteration 1
+build_iteration 2
 
 # creates required build directories
 dependency "preparation"
