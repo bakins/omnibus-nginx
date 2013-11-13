72a4e86b0192aff71f0bf1c33a37566e5b6b03d3
bakins <brian.akins@turner.com>
2013-11-13 10:20:17 -0500

    open resty 1.4.3.4

--- config/projects/nginx.rb
+++ config/projects/nginx.rb
@@ -5,7 +5,7 @@ homepage "http://openresty.org"
 
 replaces        "nginx"
 install_path    "/opt/nginx"
-build_version   '1.4.3.3'
+build_version   '1.4.3.4'
 build_iteration 1
 
 # creates required build directories
