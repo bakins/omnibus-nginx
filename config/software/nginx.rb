8ad1f5d8e1785fa9ad9c0292e20d1cfc0e55fafd
bakins <brian.akins@turner.com>
2014-01-08 07:53:21 -0500

    remove header patch

--- config/software/nginx.rb
+++ config/software/nginx.rb
@@ -35,8 +35,6 @@ build do
   command "patch -p1 < #{source_dir}/ngx_http_filter_cache/core.diff", cwd: "#{project_dir}/bundle/nginx-1.4.3"
   command "patch -p1 < #{source_dir}/nginx_upstream_check_module/check_1.2.6+.patch",  cwd: "#{project_dir}/bundle/nginx-1.4.3"
 
-  patch :source => 'server-header.diff'
-
   command ["./configure",
            "--prefix=#{install_dir}/embedded",
            "--with-luajit",
