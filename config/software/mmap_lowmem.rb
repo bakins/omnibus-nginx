d785563692e2d796d432bd7a91054aa1646cd120
bakins <brian.akins@turner.com>
2014-04-12 08:09:23 -0400

    make sure directory exists

--- config/software/mmap_lowmem.rb
+++ config/software/mmap_lowmem.rb
@@ -4,6 +4,7 @@ default_version "master"
 source git: "https://github.com/Neopallium/mmap_lowmem.git"
 
 build do
+  command "mkdir -p #{install_dir}/lib"
   command "perl -p -i -e 's/(define\s+ENABLE_VERBOSE)\s+1/$1 0/' mmap_lowmem.c"
   command "make"
   command "make install LIBDIR=#{install_dir}/lib"
